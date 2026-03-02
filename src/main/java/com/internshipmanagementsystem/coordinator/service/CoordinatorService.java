843536f02cfbe2cca0459a5ed02513106d71ebaf
Shivam Srivastava <102872098+srvstvshivam@users.noreply.github.com>
2026-03-02 17:49:25 +0530

    Delete src/main/java/com/internshipmanagementsystem/coordinator/service directory

--- src/main/java/com/internshipmanagementsystem/coordinator/service/CoordinatorService.java
+++
@@ -1,9 +0,0 @@
-package com.internshipmanagementsystem.coordinator.service;
-
-import com.internshipmanagementsystem.coordinator.dto.*;
-
-public interface CoordinatorService {
-
-    CoordinatorLoginResponse login(CoordinatorLoginRequest request);
-
-}
